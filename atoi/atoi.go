9ee124871423a3ffc75572743ae70d340cad2612
Diego Canizales <diego.canizales1995@gmail.com>
2021-07-02 14:17:44 -0500

    Improving the clamping of integer overflow tests.

--- atoi/atoi.go
+++ atoi/atoi.go
@@ -5,9 +5,9 @@ import (
 	"math"
 )
 
-func MyAtoi(s string) int32 {
+func MyAtoi(s string) int {
 
-	var res int32 = 0
+	var res = 0
 	var res64 int64 = 0
 
 	// First convert all characters to runes
@@ -19,7 +19,7 @@ func MyAtoi(s string) int32 {
 	// Ignore all leading whitespaces until first non-whitespace
 	for runes[start] == 32 {
 		if start > len(runes) {
-			return int32(0)
+			return 0
 		}
 		start++
 	}
@@ -39,9 +39,10 @@ func MyAtoi(s string) int32 {
 
 	// Check if first non-whitespace is a non-digit character
 	if runes[start] < 48 || runes[start] > 58 {
-		return int32(0)
+		return 0
 	}
 
+	fmt.Println(math.MinInt32)
 	for i := start; i < len(runes); i++ {
 		if runes[i] == 32 {
 			break
@@ -50,17 +51,13 @@ func MyAtoi(s string) int32 {
 
 			// Cast to int64 and clamp to min or max int32 to prevent overflow
 			res64 = res64*10 + int64(runes[i]-'0')
-			if res64 > math.MaxInt32 {
-				res = math.MaxInt32
-				break
+			if res64 > math.MaxInt32 || res64 < math.MinInt32 {
+				if signature == '-' {
+					return math.MinInt32 // If negative return minimal Int32
+				}
+				return math.MaxInt32 // If positive return max Int32
 			}
-			if res64 < math.MinInt32 {
-				res = math.MinInt32
-				fmt.Println(res)
-				break
-			}
-
-			res = res*10 + int32(runes[i]-'0')
+			res = res*10 + int(runes[i]-'0')
 
 		}
 	}
--- atoi/atoi_test.go
+++ atoi/atoi_test.go
@@ -1,6 +1,8 @@
 package atoi_test
 
 import (
+	"math"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -11,49 +13,49 @@ var _ = Describe("Atoi package", func() {
 
 	Describe("Atoi", func() {
 		It("Converts a string to a number", func() {
-			Expect(atoi.MyAtoi("32")).To(Equal(int32(32)))
+			Expect(atoi.MyAtoi("32")).To(Equal(32))
 		})
 	})
 
 	Describe("Atoi", func() {
 		It("Respects negative numbers", func() {
-			Expect(atoi.MyAtoi("-32")).To(Equal(int32(-32)))
+			Expect(atoi.MyAtoi("-32")).To(Equal(-32))
 		})
 	})
 
 	Describe("Atoi", func() {
 		It("Doesn't care about leading whitespaces", func() {
-			Expect(atoi.MyAtoi("      -32")).To(Equal(int32(-32)))
+			Expect(atoi.MyAtoi("      -32")).To(Equal(-32))
 		})
 	})
 
 	Describe("Atoi", func() {
 		It("Reads until first non-digit", func() {
-			Expect(atoi.MyAtoi("12341 with words")).To(Equal(int32(12341)))
+			Expect(atoi.MyAtoi("12341 with words")).To(Equal(12341))
 		})
 	})
 
 	Describe("Atoi", func() {
 		It("If it starts with non-digits, returns a zero", func() {
-			Expect(atoi.MyAtoi("with words 12341")).To(Equal(int32((0))))
+			Expect(atoi.MyAtoi("with words 12341")).To(Equal((0)))
 		})
 	})
 
 	Describe("Atoi", func() {
 		It("Treats all non-digits as zero.", func() {
-			Expect(atoi.MyAtoi("only non-digits.")).To(Equal(int32(0)))
+			Expect(atoi.MyAtoi("only non-digits.")).To(Equal(0))
 		})
 	})
 
 	Describe("Atoi", func() {
 		It("Knows how to deal with integer overflows (clamp the integer) (negative values)", func() {
-			Expect(atoi.MyAtoi("-91283472332")).To(Equal(int32(-2147483647)))
+			Expect(atoi.MyAtoi("-91283472332")).To(Equal(math.MinInt32))
 		})
 	})
 
 	Describe("Atoi", func() {
 		It("Knows how to deal with integer overflows (clamp the integer) (positive values)", func() {
-			Expect(atoi.MyAtoi("91283472332")).To(Equal(int32(2147483647)))
+			Expect(atoi.MyAtoi("91283472332")).To(Equal(math.MaxInt32))
 		})
 	})
 
