fdb90cfa3ce06734d1f87cfd74a14c663e452405
Diego Canizales <diego.canizales1995@gmail.com>
2021-07-05 14:37:00 -0500

    adding base structure for insert sort.

--- sorting/insertionsort/insertionsort_test.go
+++ sorting/insertionsort/insertionsort_test.go
@@ -3,8 +3,15 @@ package insertionsort_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	. "github.com/dbgoytia/algorithms/sorting/insertionsort"
 )
 
 var _ = Describe("InsertionSort", func() {
 
+	Describe("Insertion Sort", func() {
+		It("returns a test", func() {
+			Expect(insertionsort(2)).To(Equal(true))
+		})
+	})
 })
