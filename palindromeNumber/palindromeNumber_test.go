c95024654135596c26c395e471ec9e4cb2ee420d
Diego Canizales <diego.canizales1995@gmail.com>
2021-07-05 13:07:28 -0500

    Adding base package.

--- palindromeNumber/palindromeNumber_test.go
+++ palindromeNumber/palindromeNumber_test.go
@@ -1,17 +1,17 @@
-package palindromeNumber
+// package palindromeNumber
 
-import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+// import (
+// 	. "github.com/onsi/ginkgo"
+// 	. "github.com/onsi/gomega"
 
-	"github.com/dbgoytia/algorithms/palindromeNumber"
-)
+// 	"github.com/dbgoytia/algorithms/palindromeNumber"
+// )
 
-var _ = Describe("Palindrome number", func() {
+// var _ = Describe("Palindrome number", func() {
 
-	Describe("Palindrome number", func() {
-		It("Returns whatever", func() {
-			Expected(palindromeNumber.Something).To(Equal("just a test"))
-		})
-	})
-})
+// 	Describe("Palindrome number", func() {
+// 		It("Returns whatever", func() {
+// 			Expect(palindromeNumber.Something).To(Equal("just a test"))
+// 		})
+// 	})
+// })
