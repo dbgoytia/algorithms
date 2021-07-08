5f6e6cf6752e9ad5d40e3425b01c50d179423a3c
Diego Goytia <diego.canizales1995@gmail.com>
2021-07-08 14:16:09 -0500

    adding readme.

--- findlength/findlength_test.go
+++ findlength/findlength_test.go
@@ -14,7 +14,7 @@ var _ = Describe("Findlength", func() {
 			input := [...]int{5, 2, 4, 6, 1, 3}
 			output := [...]int{1, 2, 3, 4, 5, 6}
 
-			Expect(findlength(input[:], output[:])).To(Equal(0))
+			Expect(fl.findlength(input[:], output[:])).To(Equal(0))
 		})
 
 	})
