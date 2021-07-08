23d3263aa00b0a7c0eb73198858a996717e65634
Diego Goytia <diego.canizales1995@gmail.com>
2021-07-08 14:14:24 -0500

    adding test suite.

---
+++ findlength/findlength_suite_test.go
@@ -0,0 +1,13 @@
+package findlength_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestFindlength(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Findlength Suite")
+}
--- findlength/findlength_test.go
+++ findlength/findlength_test.go
@@ -1,20 +1,21 @@
-package findlength
+package findlength_test
 
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"github.com/dbgoytia/algorithms/findlength"
 )
 
 var _ = Describe("Findlength", func() {
 
-	Describe("Find Length", func(){
-		It("Initial testing", func(){
+	Describe("Find Length", func() {
+
+		It("Initial setup", func() {
+
 			input := [...]int{5, 2, 4, 6, 1, 3}
 			output := [...]int{1, 2, 3, 4, 5, 6}
-			Expect(findLength(input, output)).To(Equal(0))
-		})
-	})
 
+			Expect(findlength(input[:], output[:])).To(Equal(0))
+		})
+
+	})
 })
