7eff127d5f3d8421bd4bc3f03d5686d769e14570
Diego Goytia <diego.canizales1995@gmail.com>
2021-08-03 19:26:13 -0500

    Updating bfs.

--- bfsLevelOrder/bfsLevelOrder.go
+++ bfsLevelOrder/bfsLevelOrder.go
@@ -1,14 +1,5 @@
 package bfsLevelOrder
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
-
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
