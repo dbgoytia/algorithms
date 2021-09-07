38393d0d58225ae08f140720fc69615f7571e7df
Diego Goytia <diego.canizales1995@gmail.com>
2021-09-07 16:31:25 -0500

    Adding stack struct.

--- leetcode/validparenthesis/main.go
+++ leetcode/validparenthesis/main.go
@@ -1,5 +1,54 @@
 package validParenthesis
 
-func Whatever() int {
-	return 0
+import "fmt"
+
+type Stack []string
+
+// When pushing, save at the beginning of the stack
+func (s *Stack) Push(str string) {
+	*s = append(*s, str)
+}
+
+// Check for next element in stack
+func (s *Stack) Peek() string {
+
+	if s.isEmpty() {
+		return "-1"
+	}
+
+	return (*s)[len(*s)-1]
+
+}
+
+// Check if stack is empty
+func (s *Stack) isEmpty() bool {
+	return len(*s) == 0
+}
+
+// When popping, remove from the end of the stack
+func (s *Stack) Pop(str string) {
+	// Remove last element from the stack
+	index := len(*s) - 1
+	*s = (*s)[:index]
+}
+
+func IsValid(sentence string) bool {
+	var stack Stack
+	for _, i := range sentence {
+		fmt.Println(stack.Peek())
+		if string(i) == "(" || string(i) == "{" || string(i) == "[" {
+			stack.Push(string(i))
+		}
+		if string(i) == ")" || string(i) == "}" || string(i) == "]" {
+			if stack.Peek() == string(i) {
+				stack.Pop(string(i))
+			}
+		}
+	}
+
+	if stack.isEmpty() {
+		return true
+	} else {
+		return false
+	}
 }
--- leetcode/validparenthesis/validParenthesis_test.go
+++ leetcode/validparenthesis/validParenthesis_test.go
@@ -1,12 +1,38 @@
 package validParenthesis_test
 
 import (
+	"github.com/dbgoytia/algorithms/validParenthesis"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"github.com/dbgoytia/algorithms/validparenthesis"
 )
 
-var _ = Describe("Validparenthesis", func() {
+var _ = Describe("ValidParenthesis", func() {
+
+	Context("when having same parenthesis", func() {
+
+		Context("the parenthesis validator", func() {
+			It("should check if every parenthesis closes with the same kind", func() {
+				Expect(validParenthesis.IsValid("()")).Should(Equal(true))
+			})
+			It("should be able to detect differentt kind of parentheses", func() {
+				Expect(validParenthesis.IsValid("{}")).Should(Equal(true))
+			})
+			It("should detect when there's a missing closing parentheses", func() {
+				Expect(validParenthesis.IsValid("(")).Should((Equal(false)))
+			})
+		})
+	})
+
+	Context("when havin mixed parentheses", func() {
+		Context("the validator", func() {
+			It("should understand nested parentheses", func() {
+				Expect(validParenthesis.IsValid("{[]}")).Should(Equal(true))
+			})
+			It("should detect when there's a missing colsing parentheses", func() {
+				Expect(validParenthesis.IsValid("{(")).Should(Equal(false))
+			})
+
+		})
+	})
 
 })
