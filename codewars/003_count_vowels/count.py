4d493765c00e5b96fd4d2589b8641eca73ce8a70
Diego Goytia <diego.canizales1995@gmail.com>
2022-01-04 17:16:45 -0600

    one liner.

--- codewars/003_count_vowels/count.py
+++ codewars/003_count_vowels/count.py
@@ -1,7 +1,2 @@
 def get_count(sentence):
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    count = 0
-    for letter in sentence:
-        if letter in vowels:
-            count += 1
-    return count
+    return sum(1 for letter in sentence if letter in "aeiou")
