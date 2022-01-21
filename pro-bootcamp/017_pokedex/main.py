ede9c93b76a5a959ecb0e76cb1a5064fd3085205
Diego Goytia <diego.canizales1995@gmail.com>
2022-01-21 16:25:37 -0600

    Pokedex

---
+++ pro-bootcamp/017_pokedex/main.py
@@ -0,0 +1,12 @@
+from prettytable import PrettyTable
+
+pokedex = PrettyTable()
+pokedex.field_names = ["Pokemon Name", "Type"]
+
+pokedex.add_rows([
+    ["Pikachu", "electro"],
+    ["Squirtle", "water"],
+    ["Charmander", "fire"]
+])
+
+print(pokedex)
