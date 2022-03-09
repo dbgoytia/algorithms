3082e35c711ddd19703fa3705148fef88284bbf0
Diego Goytia <diego.canizales1995@gmail.com>
2022-03-08 18:18:27 -0600

    Binary Tree.

---
+++ datastructures/treeBased/binaryTree/binaryTree_test.go
@@ -0,0 +1,9 @@
+package binaryTree
+
+import (
+	"testing"
+)
+
+func TestNewNode(t *testing.T) {
+	return binaryTree.NewNode(1)
+}
---
+++ datastructures/treeBased/binaryTree/go.mod
@@ -0,0 +1,3 @@
+module github.com/dbgoytia/algorithms/datastructures/treeBased/binaryTree
+
+go 1.17
